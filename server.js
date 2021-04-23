66c8e70663dd48f862b031a71b4daa9c6f7c019f
Lucas Q <lucas.14.96@gmail.com>
2021-04-22 21:12:48 -0500

    Declared dependencies, added app.use and app.get

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
+// Declare dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const DB = require("./db/db")
 
-const express = require('express');
-
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-
-// Tells node that we are creating an "express" server
+// Express configuration and PORT 
 const app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 8080;
+
+// Links Express to assets in public folder
+app.use(express.static("public"));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
-// LISTENER
-// The below code effectively "starts" our server
-
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+// Below code handles when users visit each of the pages.
+// In each of the below cases the user is shown an HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+// Displays the notes.html page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+
+
+
+
+// app.listen to set up the server
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: http://localhost:${PORT}/`);
+});
+
+
